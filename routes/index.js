2b1d0fa49e947fc33a4f7310cbf1cf67f0f44b2c
MaryanaBOtoni <marianabotoni.com.br@gmail.com>
2024-03-28 23:31:00 -0300

    finalizado pdf 2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// Clientes
+
+router.get('/clientes', function(reeq, res){
+
+});
+
+router.post('/clientes', function(reeq, res){
+
+});
+
+router.put('/clientes', function(reeq, res){
+
+});
+
+router.delete('/clientes', function(reeq, res){
+
+});
+
+// Produtores
+
+router.get('/produtos', function(reeq, res){
+
+});
+
+router.post('/produtos', function(reeq, res){
+
+});
+
+router.put('/produtos', function(reeq, res){
+
+});
+
+router.delete('/produtos', function(reeq, res){
+
+});
+
 
 module.exports = router;
