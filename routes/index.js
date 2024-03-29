480c659962368a41b33dbf69820c015fd17e752a
MaryanaBOtoni <marianabotoni.com.br@gmail.com>
2024-03-29 00:00:06 -0300

    ajuste rotas

--- routes/index.js
+++ routes/index.js
@@ -8,20 +8,20 @@ router.get('/', function(req, res, next) {
 
 // Clientes
 
-router.get('/clientes', function(reeq, res){
-
+router.get('/clientes', function(req, res, next){
+  res.send('end-point get clientes');
 });
 
 router.post('/clientes', function(reeq, res){
-
+  res.send('end-point post clientes');
 });
 
 router.put('/clientes', function(reeq, res){
-
+  res.send('end-point put clientes');
 });
 
 router.delete('/clientes', function(reeq, res){
-
+  res.send('end-point delete clientes');
 });
 
 // Produtores
