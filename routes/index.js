c6d8d4ac39961280f0f7dc62a54a394df32f61d1
MaryanaBOtoni <marianabotoni.com.br@gmail.com>
2024-04-01 18:10:51 -0300

    Criando primeiro arquivo e integrado com npm

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,44 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+res.render('index', { title: 'Express' });
 });
 
 // Clientes
 
-router.get('/clientes', function(req, res, next){
-  res.send('end-point get clientes');
+router.get('/clientes', function(req, res, next) {
+res.send('end-point get clientes');
 });
 
-router.post('/clientes', function(reeq, res){
-  res.send('end-point post clientes');
+router.post('/clientes', function(reeq, res) {
+res.send('end-point post clientes');
 });
 
-router.put('/clientes', function(reeq, res){
-  res.send('end-point put clientes');
+router.put('/clientes', function(reeq, res) {
+res.send('end-point put clientes');
 });
 
-router.delete('/clientes', function(reeq, res){
-  res.send('end-point delete clientes');
+router.delete('/clientes', function(reeq, res) {
+res.send('end-point delete clientes');
 });
 
 // Produtores
 
-router.get('/produtos', function(reeq, res){
+router.get('/produtos', function(reeq, res) {
 
 });
 
-router.post('/produtos', function(reeq, res){
+router.post('/produtos', function(reeq, res) {
 
 });
 
-router.put('/produtos', function(reeq, res){
+router.put('/produtos', function(reeq, res) {
 
 });
 
-router.delete('/produtos', function(reeq, res){
+router.delete('/produtos', function(reeq, res) {
 
 });
 
