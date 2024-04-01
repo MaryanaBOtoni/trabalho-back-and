f185765da82709d9f689d776f4153a5eb6394ab5
MaryanaBOtoni <marianabotoni.com.br@gmail.com>
2024-04-01 20:02:16 -0300

    criando

--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,9 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
-
+/* GET home page. */
 router.get('/', function(req, res, next) {
-res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
-
-// Clientes
-
-router.get('/clientes', function(req, res, next) {
-res.send('end-point get clientes');
-});
-
-router.post('/clientes', function(reeq, res) {
-res.send('end-point post clientes');
-});
-
-router.put('/clientes', function(reeq, res) {
-res.send('end-point put clientes');
-});
-
-router.delete('/clientes', function(reeq, res) {
-res.send('end-point delete clientes');
-});
-
-// Produtores
-
-router.get('/produtos', function(reeq, res) {
-
-});
-
-router.post('/produtos', function(reeq, res) {
-
-});
-
-router.put('/produtos', function(reeq, res) {
-
-});
-
-router.delete('/produtos', function(reeq, res) {
-
-});
-
 
 module.exports = router;
